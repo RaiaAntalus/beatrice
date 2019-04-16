738d7c7cf08ca0c82d9ff4c958c197baa546832e
RaiaAntalus <raia.antalus@protonmail.com>
2019-04-16 09:06:42 +0200

    Ajout de $site_name

--- webserver_backup/scriptJson.sh
+++ webserver_backup/scriptJson.sh
@@ -2,7 +2,7 @@
 
 # Fonction qui récupère toutes les infos du site$i
 function variables_ssh() {
-  #statements
+  #
   site_address=$(jq --raw-output .site$1.site_address sites.json)
   distant_user=$(jq --raw-output .site$1.distant_user sites.json)
   backup_path=$(jq --raw-output .site$1.backup_path sites.json)
@@ -20,5 +20,5 @@ local_user=$(whoami)
 for i in `seq 1 2`
 do
   variables_ssh $i
-  ssh $distant_user@$site_address "bash -s" < ./script.sh $backup_path $mysql_pwd $mysql_user $path_to_website $local_user $local_ip
+  ssh $distant_user@$site_address "bash -s" < ./script.sh $backup_path $mysql_pwd $mysql_user $path_to_website $local_user $local_ip $site_name
 done
